e027ec2db79d49a94392d3872b2458af9703d770
santen <st3rax@gmail.com>
2015-03-19 15:43:02 +0000

    edit addUserAction

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -3,14 +3,50 @@
 namespace UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use UserBundle\Entity\User;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/index", name="index")
+     * @Route("/", name="index")
      * @Template()
      */
     public function indexAction(){
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/registration", name = "registration")
+     */
+    public function addUserAction(Request $request){
+        $mail = $request->request->get("mail");
+
+        if(strpos($mail, "@") <= 1){
+        	return $this->redirectToRoute('index');
+        }
+
+        $nickname = explode('@', $mail)[0];
+
+        $model = $this->getDoctrine()->getManager();
+
+        $user = new User();
+        $user->setNickname($nickname);
+        $user->setMail($mail);
+        $user->setPasswd(md5($request->request->get("passwd1")));
+        $user->setPosts(0);
+        $user->setCdate(date("Y-m-j"));
+
+        $model->persist($user);
+        $model->flush();
+
+        $userId = $user->getId();
+
+        setcookie("uid", $userId);
+        setcookie("sid", md5($mail));
+
+        return $this->redirectToRoute('profile', array('id' => $userId));
     }
 }
