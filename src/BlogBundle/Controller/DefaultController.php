adc409d4701d6886ec3a5c4dbfb96e7e80e8fac1
santen <st3rax@gmail.com>
2015-03-20 00:27:48 +0000

    add actions to the BlogBundle

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -6,14 +6,142 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
+use UserBundle\Entity\User;
+use BlogBundle\Entity\Post;
+use BlogBundle\Entity\Rubric;
 
 class DefaultController extends Controller
 {
 	/**
-     * @Route("/", name="index")
+     * @Route("/posts/{authorId}", name="posts")
      * @Template()
      */
-    public function indexAction(){
-        return $this->render('BlogBundle:Default:index.html.twig');
-    }    
+    public function indexAction($authorId){
+    	$author = $this->getDoctrine()->getRepository("UserBundle:user")->find($authorId);
+    	$posts = $this->getDoctrine()->getRepository("BlogBundle:post")->findBy(array('author_id' => $authorId));
+
+        return $this->render('BlogBundle:Default:index.html.twig', array('author' => $author, 'posts' => $posts));
+    }
+
+    /**
+     * @Route("/rubrics/{authorId}", name="rubrics")
+     * @Template()
+     */
+    public function getRubricsAction($authorId){
+		$rubrics = $this->getDoctrine()->getRepository("BlogBundle:rubric")->findBy(array('author_id' => $authorId));
+
+		return $this->render('BlogBundle:Default:rubrics.html.twig', array('rubrics' => $rubrics));
+    }
+
+	/**
+	 * @Route("/rubrics/create", name="newrubric")
+	 * @Template()
+	 */
+	public function addRubricAction(){
+		$userId = $_COOKIE["uid"];
+
+		$manager = $this->getDoctrine()->getManager();
+
+		$rubric = new Rubric();
+		$rubric->setRubric($request->request->get("name"));
+		$rubric->setAuthorId($authorId);
+
+		$manager->persist($rubric);
+		$manager->flush();
+
+		return $this->render('BlogBundle:Default:rubrics.html.twig');
+	}
+
+	/**
+	 * @Route("/post/create", name="newpost")
+	 * @Template()
+	 */
+	public function addPostAction(){
+		$authorId = $_COOKIE["uid"];
+
+		$manager = $this->getDoctrine()->getManager();
+		$curDate = date("Y-m-j");
+
+		$post = new Post();
+		$post->setAuthorId($authorId);
+		$post->setRubricId($request->request->get("rubric"));
+		$post->setTitle($request->request->get("title"));
+		$post->setPreview($request->request->get("preview"));
+		$post->setContent($request->request->get("content"));
+		$post->setCdate($curDate);
+
+		$manager->persist($post);
+		$manager->flush();
+
+		return $this->render('BlogBundle:Default:post.html.twig', array('post' => $post));
+	}
+
+	/**
+	 * @Route("/post/{postId}", name="newpost")
+	 * @Template()
+	 */
+	public function getPostAction($postId){
+		$manager = $this->getDoctrine()->getManager();
+		$post = $this->getDoctrine()->getRepository("BlogBundle:blog")->find($id);
+
+		$post->setViews(++$post->getViews());
+
+		$manager->persist($post);
+		$manager->flush();
+
+		return $this->render('BlogBundle:Default:post.html.twig', array('post' => $post));
+	}
+
+	/**
+	 * @Route("/post/edit/{postId}", name="editpost")
+	 * @Template()
+	 */
+	public function editPostAction($postId){		
+		$authorId = $_COOKIE["uid"];
+		$post = $this->getDoctrine()->getRepository("BlogBundle:blog")->find($id);
+
+		if($post->getAuthorId() != $authorId)
+			return $this->render('BlogBundle:Default:post.html.twig', array('postId' => $post->getId()));
+
+		return $this->render('BlogBundle:Default:editpost.html.twig', array('post' => $post));
+	}
+
+	/**
+	 * @Route("/post/save", name="savepost")
+	 * @Template()
+	 */
+	public function savePostAction(Request $request){
+		$postId = $request->request->get("id");
+
+		$manager = $this->getDoctrine()->getManager();
+		$post = $this->getDoctrine()->getRepository("BlogBundle:blog")->find($postId);
+		$curDate = date("Y-m-j");
+
+		$post = new Post();
+		$post->setRubricId($request->request->get("rubric"));
+		$post->setTitle($request->request->get("title"));
+		$post->setPreview($request->request->get("preview"));
+		$post->setContent($request->request->get("content"));
+		$post->setEdate($curDate);
+
+		$manager->persist($post);
+		$manager->flush();
+
+		return $this->render('BlogBundle:Default:post.html.twig', array('post' => $post));
+	}
+
+	/**
+	 * @Route("/post/remove/{postId}", name="removepost")
+	 * @Template()
+	 */
+	public function removePostAction($postId){
+		$userId = $_COOKIE["uid"];
+		$manager = $this->getDoctrine()->getManager();
+
+		$post->setId($postId);
+		$manager->remove($post);
+		$manager->flush();
+
+		return $this->forward('UserBundle:Default:profile', array('id' => $userId));
+	}
 }
---
+++ src/BlogBundle/Entity/Post.php~
@@ -0,0 +1,251 @@
+<?php
+
+namespace BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Post
+ */
+class Post
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var integer
+     */
+    private $authorId;
+
+    /**
+     * @var integer
+     */
+    private $rubricId;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $preview;
+
+    /**
+     * @var string
+     */
+    private $content;
+
+    /**
+     * @var \DateTime
+     */
+    private $cdate;
+
+    /**
+     * @var \DateTime
+     */
+    private $edate;
+
+    /**
+     * @var integer
+     */
+    private $views;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set authorId
+     *
+     * @param integer $authorId
+     * @return Post
+     */
+    public function setAuthorId($authorId)
+    {
+        $this->authorId = $authorId;
+
+        return $this;
+    }
+
+    /**
+     * Get authorId
+     *
+     * @return integer 
+     */
+    public function getAuthorId()
+    {
+        return $this->authorId;
+    }
+
+    /**
+     * Set rubricId
+     *
+     * @param integer $rubricId
+     * @return Post
+     */
+    public function setRubricId($rubricId)
+    {
+        $this->rubricId = $rubricId;
+
+        return $this;
+    }
+
+    /**
+     * Get rubricId
+     *
+     * @return integer 
+     */
+    public function getRubricId()
+    {
+        return $this->rubricId;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Post
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set preview
+     *
+     * @param string $preview
+     * @return Post
+     */
+    public function setPreview($preview)
+    {
+        $this->preview = $preview;
+
+        return $this;
+    }
+
+    /**
+     * Get preview
+     *
+     * @return string 
+     */
+    public function getPreview()
+    {
+        return $this->preview;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return Post
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set cdate
+     *
+     * @param \DateTime $cdate
+     * @return Post
+     */
+    public function setCdate($cdate)
+    {
+        $this->cdate = $cdate;
+
+        return $this;
+    }
+
+    /**
+     * Get cdate
+     *
+     * @return \DateTime 
+     */
+    public function getCdate()
+    {
+        return $this->cdate;
+    }
+
+    /**
+     * Set edate
+     *
+     * @param \DateTime $edate
+     * @return Post
+     */
+    public function setEdate($edate)
+    {
+        $this->edate = $edate;
+
+        return $this;
+    }
+
+    /**
+     * Get edate
+     *
+     * @return \DateTime 
+     */
+    public function getEdate()
+    {
+        return $this->edate;
+    }
+
+    /**
+     * Set views
+     *
+     * @param integer $views
+     * @return Post
+     */
+    public function setViews($views)
+    {
+        $this->views = $views;
+
+        return $this;
+    }
+
+    /**
+     * Get views
+     *
+     * @return integer 
+     */
+    public function getViews()
+    {
+        return $this->views;
+    }
+}
